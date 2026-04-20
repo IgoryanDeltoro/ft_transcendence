e1beebb41ad8ea8ae942646e3da277532b29efa4
GhRafayel <ghazarysnrafayel@gmail.com>
2026-04-20 22:44:41 +0200

    added  some routes for backend

--- beckend/src/users/users.controller.ts
+++ beckend/src/users/users.controller.ts
@@ -1,6 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller , Get, Post, Delete, Param, Body, Patch, Query} from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-    
+    constructor(private readonly UsersService: UsersService) {}
+
+    @Get() // GET /user/ get all users
+    getAllUsers(@Query('role') role?: 'PLAYER' | 'ADMIN', @Query('name') name?: string) {
+        return this.UsersService.getAllUsers(role, name);
+    }
+   
+    @Get(':id') // GET  /users/:id get user by id
+    getUserById(@Param('id') id: string) {
+        return this.UsersService.getUserById(+id);
+    }
+
+    @Post() // POST /users/ create a new user
+    createUser(@Body() user: {id: number, name: string, role: "PLAYER" | "ADMIN"})  {
+        return this.UsersService.createUser(user);
+    }
+
+    @Patch(':id') // PATCH /users/:id update user by id
+    updateUser(@Param('id') id: string, @Body() updated: {name: string, role: "PLAYER" | "ADMIN" }) {
+        return this.UsersService.updateUsser(+id, updated);
+    }
+
+    @Delete(':id') // DELETE /users/:id delete user by id 
+    deleteUser(@Param('id') id: string) {
+        return this.UsersService.delseteUser(+id);
+    }
 }
--- beckend/src/users/users.service.ts
+++ beckend/src/users/users.service.ts
@@ -1,4 +1,78 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users = [
+        {
+            id: 1,
+            name: 'John Doe',
+            role: 'ADMIN'
+        },
+        {
+            id: 2,
+            name: 'Jane Smith',
+            role: 'PLAYER'
+        },
+        {
+            id: 3,
+            name: 'Alice Johnson',
+            role: 'PLAYER'
+        },
+        {
+            id: 4,
+            name: 'Bob Brown',
+            role: 'PLAYER'
+        },
+        {
+            id: 5,
+            name: 'Charlie Davis',
+            role: 'PlAYER'
+        }
+    ]; // Leater we will replace this with database query
+
+    getAllUsers(role?: 'PLAYER' | 'ADMIN', name?: string) {
+        let res = this.users; 
+        if (role && name)
+        {
+            res = res.filter(user => user.role === role && user.name === name);
+        }
+        else if (role) {
+            res = res.filter(user => user.role === role);
+        }
+        else if (name) {
+            res = res.filter(user => user.name === name);
+        }
+        return res;
+    }
+
+    getUserById(id: number) {
+        return this.users.find(user => user.id === id);
+    }
+
+    createUser(user: {id: number, name: string, role: "PLAYER" | "ADMIN"}) {
+       let newId = 0;
+       this.users.forEach(u => {
+            if (u.id > newId)
+                newId = u.id;
+       });
+       user.id = newId + 1;
+       this.users.push(user);
+       return user;
+    }
+
+    updateUsser(id: number, updated: {name: string, role: "PLAYER" | "ADMIN" }) {
+        this.users = this.users.map(user => {
+            if (user.id === id) {
+                return { ...user, ...updated };
+            }
+            return user;
+        });
+        return this.users.find(user => user.id === id);
+    }
+
+    delseteUser(id: number) {
+        const removedUser = this.getUserById(id);
+        this.users = this.users.filter(user => user.id !== id);
+        return removedUser;
+    }
+}
