b65075818f06fc2177e8fae6e050f400f9448fe6
GhRafayel <ghazarysnrafayel@gmail.com>
2026-05-01 13:28:03 +0200

    impufe the input logics

--- frontend/app/Components/registration/page.tsx
+++ frontend/app/Components/registration/page.tsx
@@ -1,10 +1,17 @@
 "use client"
 import { useEffect, useState } from "react"
 
+let index = 1;
+function clear(array: Item[]) : Item[] {
+	return array.map((item) => ({
+		...item,
+		value: "",
+		bol: true,
+	}))
+}
 function Registration() {
 
-	const fromStyles = 
-	{
+	const fromStyles =  {
 		inputDiv: "flex justify-between border-b-black border-b-2 max-5 my-7 py-1 m-4",
 		inputs: "w-11/12 bg-transparent outline-none placeholder-black text-3xl placeholder:text-xl",
 		imgDiv: "w-2/12 flex items-center justify-center",
@@ -56,7 +63,7 @@ function Registration() {
 
 return (
 
-	<div  className="bg w-full   h-screen flex items-center justify-center" >
+	<div  className="bg w-full h-screen flex items-center justify-center" >
 
 		<div className="w-full max-w-md mx-auto p-6 glass rounded-2xl">
 
@@ -66,33 +73,27 @@ return (
 				</h2>
 			</div>
 			
-			<form action="">
-				
+			<form action="submit" onSubmit={(e) => {
+				e.preventDefault();
+				if (isLoginMode && labelFocus[labelFocus.length - 1].value != labelFocus[labelFocus.length - 2].value)
+				{
+					setLabelFocus(clear(labelFocus));
+					return ;
+				}
+				// setLabelFocus(clear(labelFocus));
+			}}>
 				{labelFocus.map((item, i) => {
 					if (isLoginMode && i === 0)
-							return null;
+							return ;
 					return (
 						<div className={fromStyles.inputDiv} key={i}>
 							
 							<label htmlFor={item.name} className="cursor-pointer">
-								<input 
-									placeholder={item.bol ? item.name : ""}
+								<input required placeholder={item.bol ? item.name : ""}
 									type={item.type} name={item.name} id={String(i)} value={item.value} className={fromStyles.inputs}
-									onFocus={(e) => {
-										const index = Number(e.target.id);
-										labelFocus[index].bol = !labelFocus[index].bol;
-										setLabelFocus([...labelFocus])
-									}}
-									onChange={(e) => {
-										const index = Number(e.target.id);
-										labelFocus[index].value = e.target.value;
-										setLabelFocus([...labelFocus])
-									}}
-									onBlur={(e) => {
-										const index = Number(e.target.id);
-										labelFocus[index].bol = !labelFocus[index].bol;
-										setLabelFocus([...labelFocus])
-									}}
+									onFocus={(e) => { setLabelFocus((prev) => prev.map((item, i) => i === Number(e.target.id) ? {...item, bol: false} : item)); }}
+									onChange={(e) => { setLabelFocus((prev) => prev.map((item, i) => i ===  Number(e.target.id) ? {...item, value: e.target.value} : item)); }}
+									onBlur={(e) => { setLabelFocus((prev) => prev.map((item, i) => i ===  Number(e.target.id) ? {...item, bol: true} : item)); }}
 								/>
 							</label>
 							<div className={fromStyles.imgDiv}>
@@ -104,23 +105,19 @@ return (
 				
 				<div className="m-5 text-center">
 					<div>
-						<button className="border cursor-pointer w-1/2 text-center rounded-2xl font-bold hover:bg-blue-400 transition-all hover:text-blue-200 hover:border-gray-400"
+						<button className="border border-blue-300  cursor-pointer w-2/2 text-center text-lg p-3 rounded-2xl font-bold hover:bg-blue-400 transition-all hover:text-blue-100 hover:border-gray-800"
 							type="submit" onSubmit={() => {
 							labelFocus.forEach((item) => {
 								console.log(item.value)
 							})
 						}}> {isLoginMode ? "Login" : "Sign Up"} </button>
 					</div>
-					<div className="p-4 text-lg font-bold">
+					<div className="p-4 text-lg font-bold ">
 						<p>
 							{!isLoginMode ? "Already have an account? / " : "Don't have an account? / " }
-							<a 
-								onClick={() => {
-									setIsLoginMod(!isLoginMode);
-									console.log("I should write a login later ");
-								}}
-								href="#">{!isLoginMode ? " Login" : " Sign Up"}
-							</a>
+							<button type="button" onClick={() => { setIsLoginMod(!isLoginMode); }} >
+								{!isLoginMode ? " Login" : " Sign Up"}
+							</button>
 						</p>
 					</div>
 				</div>
