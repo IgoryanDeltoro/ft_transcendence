6f91d57cf9d047f65f0ac971cdbf7b7b81cd4ae9
GhRafayel <ghazarysnrafayel@gmail.com>
2026-05-02 19:43:33 +0200

    added password open and close different icons

--- frontend/app/Components/registration/page.tsx
+++ frontend/app/Components/registration/page.tsx
@@ -1,10 +1,10 @@
 "use client"
-import { useEffect, useState } from "react"
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react"
 
-let data = {
-	 struct: [
+let data = [
                     {
+						id: "1",
                         type: "text",
                         name: "Username",
                         src : "/png/iconUsers.png",
@@ -12,6 +12,7 @@ let data = {
                         bol: true,
                     },
                     {
+						id: "2",
                         type: "email",
                         name: "Email",
                         src : "/png/iconEmail.png",
@@ -19,30 +20,29 @@ let data = {
                         bol: true,
                     },
                     {
+						id: "3",
                         type: "password",
                         name: "Password",
                         src : "/png/iconSecret.png",
                         value: "",
                         bol: true,
                     }
-            ],
-	fromStyles: 
-        {
-            inputDiv: "flex justify-between border-b-black border-b-2 max-5 my-7 py-1 m-4",
-            inputs: " bg-transparent outline-none placeholder-black text-3xl placeholder:text-xl",
-            imgDiv: " flex items-center justify-center",
-            btn_submit:  "border border-blue-300 rounded-2xl cursor-pointer text-center text-lg font-bold hover:bg-blue-400 transition-all duration-600 hover:text-blue-100 hover:border-gray-800 p-3 w-2/2 ",
-			btn_sin_log: "border border-blue-300 rounded-2xl cursor-pointer text-center text-lg font-bold hover:bg-blue-400 transition-all duration-600 hover:text-blue-100 hover:border-gray-800 text-center p-3 py-1"
-        },
-       
+            ];
+let fromStyles = 
+{
+	inputDiv: "flex justify-between border-b-black border-b-2 max-5 my-7 py-1 m-4",
+	inputs: " bg-transparent outline-none placeholder-black text-3xl placeholder:text-xl",
+	imgDiv: " flex items-center justify-center",
+	btn_submit:  "border border-blue-300 rounded-2xl cursor-pointer text-center text-lg font-bold hover:bg-blue-400 transition-all duration-600 hover:text-blue-100 hover:border-gray-800 p-3 w-2/2 ",
+	btn_sin_log: "border border-blue-300 rounded-2xl cursor-pointer text-center text-lg font-bold hover:bg-blue-400 transition-all duration-600 hover:text-blue-100 hover:border-gray-800 text-center p-3 py-1"
 }
+
 function Registration() {
 
-	const [isLoginMode, setIsLoginMod] = useState(true);
-	const [labelFocus, setLabelFocus] = useState(data.struct);
-	const router = useRouter();
 
-	useEffect(() => { router.refresh(); }, [router])
+	const rout = useRouter();
+	const [isLoginMode, setIsLoginMod] = useState(true);
+	const [labelFocus, setLabelFocus] = useState(data);
 	
 	useEffect(() => {
 		if (isLoginMode && labelFocus.length === 4)
@@ -51,21 +51,23 @@ function Registration() {
 		}
 		else if (!isLoginMode && labelFocus.length < 4)
 		{
-			setLabelFocus((prev) => [...prev, {
-                        type: "password",
-                        name: "ConfirmPassword",
-                        src : "/png/iconSecret.png",
-                        value: "",
-                        bol: true,
-                    }]);
+			setLabelFocus((prev) => [...prev, 
+				{
+					id: '4',
+					type: "password",
+					name: "ConfirmPassword",
+					src : "/png/iconSecret.png",
+					value: "",
+					bol: true,
+                }]);
 		}
 	}, [isLoginMode]);
 
 return (
 
-	<div  className="bg w-full h-screen flex items-center justify-center" >
+	<div  className="bg w-full h-screen flex min-w-md items-center justify-center" >
 
-		<div className="w-full max-w-md mx-auto p-6 glass rounded-2xl">
+		<div className="w-full max-w-md min-w-md mx-auto p-6 glass rounded-2xl">
 
 			<div className="w-full text-center my-3">
 				<h2 className="text-4xl font-bold">
@@ -98,33 +100,51 @@ return (
 					})
 				})
 				.then((res) => {
-					if (res.ok)
+					if (res.ok && !isLoginMode)
 					{
 						setLabelFocus((prev) => prev.map((item) => ({...item, value: "", bol: true})));
 						setIsLoginMod(true);
 					}
+					else if (res.ok && isLoginMode)
+						rout.push('/');
 					else
+					{
 						alert("");
+					}
 				})
-				
-				
 			}}>
 				{labelFocus.map((item, i) => {
 					if (isLoginMode && i === 0)
-							return ;
+							return null;
 					return (
-						<div className={data.fromStyles.inputDiv} key={i}>
-							
-							<label htmlFor={String(i)} className="cursor-pointer">
+						<div className={fromStyles.inputDiv} key={i}>
+							<label htmlFor={item.name} className="cursor-pointer">
 								<input required placeholder={item.bol ? item.name : ""}
-									type={item.type} name={item.name} id={String(i)} value={item.value} className={data.fromStyles.inputs}
-									onFocus={(e) => { setLabelFocus((prev) => prev.map((item, i) => i === Number(e.target.id) ? {...item, bol: false} : item)); }}
-									onChange={(e) => { setLabelFocus((prev) => prev.map((item, i) => i ===  Number(e.target.id) ? {...item, value: e.target.value} : item)); }}
-									onBlur={(e) => { setLabelFocus((prev) => prev.map((item, i) => i ===  Number(e.target.id) ? {...item, bol: true} : item)); }}
+									type={item.type} name={item.name} id={item.id} value={item.value} className={fromStyles.inputs}
+									onFocus={(e) => { setLabelFocus((prev) => prev.map((item) => item.id === e.target.id ? {...item, bol: false} : item)); }}
+									onChange={(e) => { setLabelFocus((prev) => prev.map((item) => item.id === e.target.id ? {...item, value: e.target.value} : item)); }}
+									onBlur={(e) => { setLabelFocus((prev) => prev.map((item) => item.id === e.target.id ? {...item, bol: true} : item)); }}
 								/>
+								
 							</label>
-							<div className={data.fromStyles.imgDiv}>
-								<img src={`${item.src}`} alt="icon" className="w-15  min-w-8"/>
+							<div className={fromStyles.imgDiv}>
+								<img src={`${item.src}`} alt="icon" id={item.id} className="w-15  min-w-8 cursor-pointer"
+									onClick={(e) => {
+										setLabelFocus((prev) => (
+											prev.map((item) => {
+												if (item.id === e.target.id && (item.name === "Password" || item.name === "ConfirmPassword"))
+												{
+													if (item.type === "text")
+														return {...item, type: "password", src: "/png/iconSecret.png"}
+													else
+														return {...item, type: "text", src: "/png/iconEye.png"}
+												}
+												else
+													return item;
+											})
+										));
+									}}
+								/>
 							</div>
 						</div>
 					)
@@ -132,15 +152,15 @@ return (
 				
 				<div className="m-5 text-center">
 					<div>
-						<button className={data.fromStyles.btn_submit} type="submit" > 
+						<button className={fromStyles.btn_submit} type="submit" > 
 							{isLoginMode ? "Login" : "Sign Up"}
 						</button>
 					</div>
 					<div className="p-4 text-lg font-bold flex justify-between">
 						<p>
 							{!isLoginMode ? "Already have an account ? / " : "Don't have an account ? / " }
-							<button  className={data.fromStyles.btn_sin_log}
-								type="button" onClick={() => { 
+							<button  className={fromStyles.btn_sin_log}
+								type="button" onClick={() => {
 								setIsLoginMod(!isLoginMode); 
 								setLabelFocus((prev) => prev.map((item) => ({...item, value: "", bol: true})));
 								}} >
@@ -149,6 +169,7 @@ return (
 						</p>
 					</div>
 				</div>
+
 			</form>
 		</div>
 	</div>
